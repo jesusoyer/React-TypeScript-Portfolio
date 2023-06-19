c0596034aa124cca6af8c592517f5347ef07461c
jesusoyer <oyervidesjesus017@gmail.com>
2023-06-19 12:24:54 -0500

    new react typescript portfolio project

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="md:container md:mx-auto text-center ">
+      practice
+      
+    </div>
+  );
+}
+
+export default App;
